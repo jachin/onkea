40c6281c48b65700d6f61340451067e059940edd
Jachin Rupe <jachin@jachin.rupe.name>
2026-03-20 21:07:28 -0500

    Initial Commit

---
+++ YouGoHugo/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  YouGoHugo
+//
+//  Created by Jachin Rupe on 3/20/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ YouGoHugoTests/YouGoHugoTests.swift
@@ -0,0 +1,17 @@
+//
+//  YouGoHugoTests.swift
+//  YouGoHugoTests
+//
+//  Created by Jachin Rupe on 3/20/26.
+//
+
+import Testing
+@testable import YouGoHugo
+
+struct YouGoHugoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
